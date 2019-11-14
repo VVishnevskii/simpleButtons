f4a94f03729020ce93e66bd89265b686ccc627a2
VVishnevskii <50050949+VVishnevskii@users.noreply.github.com>
2019-11-14 10:20:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
-var buttonPageHeader_1 = document.querySelector('.button_1');
+let buttonPageHeader_1 = document.querySelector('.button_1');
 
 buttonPageHeader_1.addEventListener('click', function() {
-  var buttonStatus_1 = document.querySelector('.active_1');
-    if (buttonStatus_1) {
-      buttonPageHeader_1.classList.remove('active_1');
-    }
-  else {
-    buttonPageHeader_1.classList.add('active_1');
-  }
+  let buttonStatus_1 = document.querySelector('.active_1');
+    buttonPageHeader_1.classList.toggle('active_1');
 })
 
-var buttonPageHeader_2 = document.querySelector('.button_2');
+let buttonPageHeader_2 = document.querySelector('.button_2');
 
 buttonPageHeader_2.addEventListener('click', function() {
-  var buttonStatus_2 = document.querySelector('.active_2');
-    if (buttonStatus_2) {
-      buttonPageHeader_2.classList.remove('active_2');
-    }
-  else {
-    buttonPageHeader_2.classList.add('active_2');
-  }
+  let buttonStatus_2 = document.querySelector('.active_2');
+    buttonPageHeader_2.classList.toggle('active_2');
 })
 
-var buttonPageHeader_3 = document.querySelector('.button_3');
+let buttonPageHeader_3 = document.querySelector('.button_3');
 
 buttonPageHeader_3.addEventListener('click', function() {
-  var buttonStatus_3 = document.querySelector('.active_3');
-    if (buttonStatus_3) {
-      buttonPageHeader_3.classList.remove('active_3');
-    }
-  else {
-    buttonPageHeader_3.classList.add('active_3');
-  }
+  let buttonStatus_3 = document.querySelector('.active_3');
+    buttonPageHeader_3.classList.toggle('active_3');  
 })
 
-var buttonPageHeader_4 = document.querySelector('.button_4');
+let buttonPageHeader_4 = document.querySelector('.button_4');
 
 buttonPageHeader_4.addEventListener('click', function() {
-  var buttonStatus_4 = document.querySelector('.active_4');
-    if (buttonStatus_4) {
-      buttonPageHeader_4.classList.remove('active_4');
-    }
-  else {
-    buttonPageHeader_4.classList.add('active_4');
-  }
+  let buttonStatus_4 = document.querySelector('.active_4');
+    buttonPageHeader_4.classList.toggle('active_4');
 })
